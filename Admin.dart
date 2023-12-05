56ead535d4ba51bf9fc48e9b9e46acf1ef5c49cf
fitriya20 <fy043259@gmail.com>
2023-12-05 15:35:47 +0700

    baru pipit

---
+++ Admin.dart
@@ -0,0 +1,19 @@
+class Admin {
+  String? nama;
+  String? alamat;
+  String? email;
+
+  Admin(this.nama, this.alamat, this.email);
+
+  String getNama() {
+    return nama;
+  }
+
+  String getAlamat() {
+    return alamat;
+  }
+
+  String getEmail() {
+    return email;
+  }
+}

db0d8bd12afd5a8676d9d314bfdbc88f8c7512da
bimaryan <bimaryan046@gmail.com>
2023-12-05 15:33:26 +0700

    update

--- distributor.dart
+++ distributor.dart
@@ -21,29 +21,3 @@ class Distributor {
   set setAlamat(String alamat) => this.alamat = alamat;
   set setPhoneNumber(String phoneNumber) => this.phoneNumber = phoneNumber;
 }
-
-void main() {
-  // Contoh penggunaan class Distributor
-  Distributor distributor = Distributor(1, 'Distributor A',
-      'distributor@example.com', 'Jl. Contoh No. 123', '081234567890');
-
-  // Mendapatkan nilai atribut
-  print('ID: ${distributor.getId}');
-  print('Nama: ${distributor.getNama}');
-  print('Email: ${distributor.getEmail}');
-  print('Alamat: ${distributor.getAlamat}');
-  print('Phone Number: ${distributor.getPhoneNumber}');
-
-  // Mengubah nilai atribut
-  distributor.setNama = 'Distributor Baru';
-  distributor.setEmail = 'baru@example.com';
-  distributor.setAlamat = 'Jl. Baru No. 456';
-  distributor.setPhoneNumber = '081234567891';
-
-  // Menampilkan nilai atribut setelah diubah
-  print('\nSetelah diubah:');
-  print('Nama: ${distributor.getNama}');
-  print('Email: ${distributor.getEmail}');
-  print('Alamat: ${distributor.getAlamat}');
-  print('Phone Number: ${distributor.getPhoneNumber}');
-}

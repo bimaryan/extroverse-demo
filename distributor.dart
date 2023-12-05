fc191474000b71cf08d37ca2b4ccc3f4ededf669
bimaryan <bimaryan046@gmail.com>
2023-12-05 15:20:19 +0700

    hapus

---
+++ distributor.dart
@@ -0,0 +1,16 @@
+class Distributor{
+  int? _id;
+  String? _nama;
+  String? _alamat;
+  int? _phonenumber;
+
+  //konstruktor
+  Distributor(this._id, this._nama, this._alamat, this._phonenumber);
+
+  //getter dan setter untuk nama
+  String get nama => _nama;
+  set nama(String nama) => _nama = nama;
+
+  //getter dan setter untuk email
+  
+}
